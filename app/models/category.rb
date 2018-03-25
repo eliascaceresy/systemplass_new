28a45ef6feb0f72a8696328668169807a30ff7f2
Elias Caceres <elias.caceres@ceinf.cl>
2018-03-25 01:03:53 -0300

    relaciones en los modelos

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :products
+  has_many :subcategories, inverse_of: :category
+  accepts_nested_attributes_for :subcategories, :reject_if => :all_blank, :allow_destroy => true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
-class Product < ApplicationRecord
+class Product < ApplicationRecordry
+  belongs_to :category
+  belongs_to :subcategory,optional:true
+  has_many :pictures,inverse_of: :product,dependent: :destroy
+  accepts_nested_attributes_for :pictures, :reject_if => :all_blank, :allow_destroy => true
+
+  validates :name,presence:true
+  validates :price,presence:true
+  validates :description,presence:true
+  validates :category_id, presence:true
 end
