27c081e069af2595d1eaa4a0d0a1c5990576e918
Elias Caceres <elias.jcy@gmail.com>
2019-03-19 14:03:00 -0300

    [Cambio de marca]
    - se inicia el cambio de marca de systemplas
    
    this close #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
 
-  resources :quotations,only:[:create]
-  resources :products,only:[:index,:show]
-
-  devise_for :users, path_names: { sign_in: 'login'}
+  # resources :quotations,only:[:create]
+  # resources :products,only:[:index,:show]
+  #
+  # devise_for :users, path_names: { sign_in: 'login'}
   root "main#home"
-  get '/about',to: 'main#about'
-  get '/contact',to: 'main#contact'
-  get '/services',to: 'main#services'
-  get '/admin',to: 'admin#index'
-  resources :products,only:[:index,:show]
-
-  namespace :administration do
-    resources :categories,except:[:destroy]
-    resources :products
-    resources :quotations,only:[:index,:show]
-  end
+  # get '/about',to: 'main#about'
+  # get '/contact',to: 'main#contact'
+  # get '/services',to: 'main#services'
+  # get '/admin',to: 'admin#index'
+  # resources :products,only:[:index,:show]
+  #
+  # namespace :administration do
+  #   resources :categories,except:[:destroy]
+  #   resources :products
+  #   resources :quotations,only:[:index,:show]
+  # end
 end
